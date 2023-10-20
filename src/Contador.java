1526d2ea8571e2724f5e94dbf2dd7531caba3a4f
JoaoPauuloCarneiro <joopaulocarneiro85@gmail.com>
2023-10-20 20:51:26 -0300

    primeiro commit

---
+++ src/Contador.java
@@ -0,0 +1,33 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro: ");
+		int parametroUm = terminal.nextInt ();
+		System.out.println("Digite o segundo parâmetro: ");
+		int parametroDois = terminal.nextInt ();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException e) {
+            System.out.println("Erro" + e.getMessage());
+		
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException ();
+        } else {
+            int contar = parametroDois - parametroUm;
+        for (int i = 1; i <= contar; i++) {
+            System.out.println ("Imprimindo: " +i);
+        }
+        }
+		
+	}
+}
