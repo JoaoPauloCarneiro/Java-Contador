ae17c89be79d79731642922b085492195adb051e
João Paulo <142929989+JoaoPauloCarneiro@users.noreply.github.com>
2023-10-20 20:54:03 -0300

    Update ParametrosInvalidosException.java

--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,6 +1,6 @@
 public class ParametrosInvalidosException extends Exception {
     public ParametrosInvalidosException () {
-        super(" O Segundo Parametro deverá ser Maior que o Primeiro. ");
+        super(" O Segundo Parametro deverá ser Maior que o Primeiro!!!!");
     }
     
 }
